583eee1e02d46d8bf655600798d424e91916c8cf
Akram Klai <akram.klai@mail.utoronto.ca>
2024-11-24 15:45:20 -0500

    Fixed errors with DashboardUI. Should be working properly for both forcast and historical weather data.

--- src/main/java/application/dto/WeatherDataDTO.java
+++ src/main/java/application/dto/WeatherDataDTO.java
@@ -13,44 +13,45 @@ public class WeatherDataDTO {
     public Location location;
     public LocalDate startDate;
     public LocalDate endDate;
-    public double temperature;
-    public int humidity;
-    public double windSpeed;
-    public double windDirection;
-    public double precipitation;
+    // These represent all the weather details over the given time interval as lists by category of data
+    public JSONArray temperature;
+    public JSONArray humidity;
+    public JSONArray windSpeed;
+    public JSONArray windDirection;
+    public JSONArray precipitation;
     public List<String> alerts;
 
     // Constructor for mapping from domain entities
-    public WeatherDataDTO(Location location, List<LocalDate> timeInterval, Map<String, Double> weatherDetails, List<String> alerts) {
+    public WeatherDataDTO(Location location, List<LocalDate> timeInterval, Map<String, JSONArray> weatherDetails, List<String> alerts) {
 
         this.location = location;
         this.startDate = timeInterval.get(0);
         this.endDate = timeInterval.get(timeInterval.size() - 1);
         this.temperature = weatherDetails.get("temperature");
-        this.humidity = weatherDetails.get("humidity").intValue();
+        this.humidity = weatherDetails.get("humidity");
         this.windSpeed = weatherDetails.get("windSpeed");
         this.windDirection = weatherDetails.get("windDirection");
         this.precipitation = weatherDetails.get("percipitation");
         this.alerts = alerts;
     }
 
-    public double getTemperature(String type) {
+    public double getTemperature(String type, int index) {
         switch (type) {
             case "kel":
-                return getTemperatureInKelvin();
+                return getTemperatureInKelvin(index);
             case "fah":
-                return getTemperatureInFahrenheit();
+                return getTemperatureInFahrenheit(index);
             default:
-                return temperature;
+                return temperature.getDouble(index);
         }
     }
 
-    public double getTemperatureInKelvin() {
-        return this.temperature + Constants.CELSIUS_TO_KELVIN_OFFSET;
+    public double getTemperatureInKelvin(int index) {
+        return this.temperature.getDouble(index) + Constants.CELSIUS_TO_KELVIN_OFFSET;
     }
 
-    public double getTemperatureInFahrenheit() {
-        return this.temperature * Constants.CELSIUS_TO_FAHRENHEIT_FACTOR + Constants.CELSIUS_TO_FAHRENHEIT_OFFSET;
+    public double getTemperatureInFahrenheit(int index) {
+        return this.temperature.getDouble(index) * Constants.CELSIUS_TO_FAHRENHEIT_FACTOR + Constants.CELSIUS_TO_FAHRENHEIT_OFFSET;
     }
 
     /**
--- src/main/java/application/dto/WeatherDataDTOGenerator.java
+++ src/main/java/application/dto/WeatherDataDTOGenerator.java
@@ -1,10 +1,12 @@
 package application.dto;
 
 import domain.entities.Location;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import domain.entities.WeatherData;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,20 +16,20 @@ import java.util.Map;
 
 // Generates the weather data object to be used by the dashboard and rest of application
 public class WeatherDataDTOGenerator {
-    public WeatherDataDTO createWeatherDataDTO(WeatherData weatherData, Location location, LocalDate startDate, LocalDate endDate){
+    public static WeatherDataDTO createWeatherDataDTO(WeatherData weatherData, Location location, LocalDate startDate, LocalDate endDate){
         JSONObject data = weatherData.getWeatherDetails();
         List<LocalDate> dates = new ArrayList<>();
-        Map<String, Double> weatherDetails  = new HashMap<>();
+        Map<String, JSONArray> weatherDetails  = new HashMap<>();
         dates.add(startDate);
         dates.add(endDate);
         try {
             JSONObject dailyData = data.getJSONObject("daily");
             JSONObject hourlyData = data.getJSONObject("hourly");
-            weatherDetails.put("temperature", WeatherDataDTO.getAverageData(hourlyData.getJSONArray("temperature_2m")));
-            weatherDetails.put("humidity", WeatherDataDTO.getAverageData(hourlyData.getJSONArray("relative_humidity_2m")));
-            weatherDetails.put("windSpeed", dailyData.getJSONArray("wind_speed_10m_max").getDouble(0));
-            weatherDetails.put("windDirection", dailyData.getJSONArray("wind_direction_10m_dominant").getDouble(0));
-            weatherDetails.put("percipitation", (double) dailyData.getJSONArray("precipitation_sum").getInt(0));
+            weatherDetails.put("temperature", hourlyData.getJSONArray("temperature_2m"));
+            weatherDetails.put("humidity", hourlyData.getJSONArray("relative_humidity_2m"));
+            weatherDetails.put("windSpeed", dailyData.getJSONArray("wind_speed_10m_max"));
+            weatherDetails.put("windDirection", dailyData.getJSONArray("wind_direction_10m_dominant"));
+            weatherDetails.put("percipitation", dailyData.getJSONArray("precipitation_sum"));
             return new WeatherDataDTO(location, dates, weatherDetails, new ArrayList<String>(){} );
             //NOTE: Don't have alerts (for now)
         }
