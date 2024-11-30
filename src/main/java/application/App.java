a512bde61c3db568bfea545a8f8afc41dcde6f69
Reena <rmobmina@gmail.com>
2024-11-30 17:39:48 -0500

    error response code 401?

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,3 +1,25 @@
+/**************************************************************************************************
+ * CSC207 PROJECT: THE WEATHER APP
+ *
+ * Module Description
+ * ===================
+ *
+ * This module is part of The Weather App project for CSC207: Software Design.
+ * The code herein contributes to the overall functionality of the application and adheres
+ * to design principles and documentation standards. Proper attribution is given where
+ * external resources are used, and all functions include comprehensive explanations.
+ *
+ * Copyright and Usage Information
+ * ================================
+ *
+ * This file is provided solely for the personal and private use of CSC207 students,
+ * teaching staff, and the Department of Computer Science at the University of Toronto
+ * St. George campus. Any form of distribution of this code, whether as provided or
+ * with modifications, is strictly prohibited.
+ *
+ * This file is Copyright (c) Akram Klai, Reena Obmina, Edison Yao, Bader Aleisari, Arham Mohammed.
+ **************************************************************************************************/
+
 package application;
 
 import infrastructure.adapters.OpenWeatherApiService;
